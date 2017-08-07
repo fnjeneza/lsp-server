8a8c2e31ab1e71d8baafc7807615b04a1a41a2af
Francois Njeneza <njenezamed@gmail.com>
2017-08-07 18:56:14 +0200

    add show messqage request params

--- ShowMessageParams.py
+++ ShowMessageParams.py
@@ -4,6 +4,9 @@ class MessageType(object):
     Info = 3
     Log = 4
 
+class MessageActionItem(object):
+    title = ""
+
 class ShowMessageParams(object):
     def __init__(self, type_, message):
         if(type_ is not None):
@@ -12,4 +15,15 @@ class ShowMessageParams(object):
         if message is not None:
             assert isinstance(message, str)
             self._message = message
+
+
+class ShowMessageRequestParams(ShowMessageParams):
+    def __init__(self, type_, message, actions):
+        super(type_, message)
+
+        if actions is not None:
+            assert isinstance(actions, MessageActionItem)
+            self._actions = actions
+
+
 
