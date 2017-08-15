e69aef2eaedec01b34a1c12a779b89668f557aa5
Francois Njeneza <njenezamed@gmail.com>
2017-08-15 17:45:13 +0200

    run server forever

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ PORT=8888
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST,PORT))
     s.listen(1)
-    conn, addr = s.accept()
+    # run server forever
+    while True:
+        # wait for a connection
+        conn, addr = s.accept()
 
-    with conn:
-        print("connected to {}".format(addr))
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+        with conn:
+            print("connected to {}".format(addr))
+            while True:
+                data = conn.recv(1024)
+                if not data: break
+                conn.sendall(data)
